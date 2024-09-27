7dc866cb175b17b60238a9c183ec7025564b5e5b
opilane <opilane@HP-CZC321706C>
2024-09-27 16:45:32 +0300

    index
    index

---
+++ ContosoUniverstity/Controllers/DelinquentController.cs
@@ -0,0 +1,24 @@
+﻿using ContosoUniverstity.Data;
+using ContosoUniverstity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniverstity.Controllers
+{
+    public class DelinquetController : Controller
+    {
+        public readonly SchoolContext _context;
+
+        public DelinquetController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var delinquents = await _context.Delinquents.ToListAsync();
+            return View(delinquents);
+        }
+
+
+    }
+}
---
+++ ContosoUniverstity/Models/Delinquent.cs
@@ -0,0 +1,20 @@
+﻿namespace ContosoUniverstity.Models
+{
+    public class Delinquent
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public RecentViolation? Violation { get; set; }
+    }
+
+    public enum RecentViolation
+    {
+        CleanRecord,
+        MinorInfraction,
+        SeriousInfraction,
+        MajorInfraction,
+        Expelled
+    }
+}
