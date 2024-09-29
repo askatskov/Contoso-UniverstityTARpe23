0435d979657222be501326c3c99bea07b019a803
Artjom Skatshkov <awesarshtjau@gmail.com>
2024-09-30 01:20:53 +0300

    indexisse lisatud Baseon

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756c5fd3128efc387262b80edd9e7ef1b41e6a58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040a5223082b134216130aa61b42ff6d09fe18be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
