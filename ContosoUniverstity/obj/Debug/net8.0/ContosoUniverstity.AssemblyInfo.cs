18b3c0f2e67c07edba92410da58317bbb5aa9cf4
opilane <opilane@HP-CZC321705F>
2024-09-30 14:10:18 +0300

    edit.cshtml

--- ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
+++ ContosoUniverstity/obj/Debug/net8.0/ContosoUniverstity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9106aafb58c555eb704588df03ec66bdb67b83de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4705a07f548755fee4ec6cdc4add0b4054afae94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniverstity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
