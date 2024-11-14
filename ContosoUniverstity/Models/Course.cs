d4770e14bd44ea70f064d0600307f4e984b4e758
awesa <awesa@DESKTOP-L09ETCB>
2024-11-14 19:33:05 +0200

    Lisatud
    CreateEdit - puhas
    DetailsDelete - puhas
    Index - tehtud

--- ContosoUniverstity/Models/Course.cs
+++ ContosoUniverstity/Models/Course.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using ContosoUniverstity.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniverstity.Models
 {
